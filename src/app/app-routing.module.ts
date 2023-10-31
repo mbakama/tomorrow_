42b021910f7f05e8e496a47352e8bcd52c2495e0
hector mbakama bope <hector.mbakama@hologram.cd>
2023-10-31 01:39:37 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MusicComponent } from './pages/music/music.component';
+import { VideoComponent } from './pages/video/video.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ArtisteComponent } from './pages/artiste/artiste.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'music', component: MusicComponent },
+  { path: 'video', component: VideoComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'artiste', component: ArtisteComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'blog', component: BlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MusicComponent } from './pages/music/music.component';
+import { VideoComponent } from './pages/video/video.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ArtisteComponent } from './pages/artiste/artiste.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MusicComponent,
+    VideoComponent,
+    AboutComponent,
+    ServicesComponent,
+    ArtisteComponent,
+    ContactComponent,
+    BlogComponent, 
+    HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
